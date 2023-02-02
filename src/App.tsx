9a2060aa565d6d9fcbf3a595e5fa2f44227dd358
Alyssa Sitto <alyssasitto1@gmail.com>
2023-02-01 21:03:37 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+function App() {
+	return (
+		<Container>
+			<Routes></Routes>
+		</Container>
+	);
+}
+
+export default App;
